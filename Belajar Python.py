41bda3439c457c6c81533c70ad9f2e44352d6673
Didray <daewyh@gmail.com>
2023-08-17 20:39:43 -0700

    Create Belajar Python.py

---
+++ Belajar Python.py
@@ -0,0 +1,19 @@
+nama = 'Dimas Solar'
+Usia = 18
+tinggi_badan = 200.5
+
+menyapa = 'Saya member organisasi sungut lele. Nama saya '
+skill = '. Saya bisa berhitung. contoh: '
+
+a = 20
+b = 30
+Hasil = a * b
+
+print(menyapa + nama + '. Umur gua ' + str(Usia) + skill + "20 * 30 hasilnya " + str(Hasil))
+print('Coba tes gua soal matematika.')
+
+c = input('Coba tulis angka pertama:') #outputnya merupakan str
+d = input('dikali angka kedua:') #ini juga outputnya str
+Hasil2 = int(c) * int(d)
+
+print('Hasilnya adalah ' + str(Hasil2))
